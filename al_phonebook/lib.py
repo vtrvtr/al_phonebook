230b1f8f682b67a1f4249aa8399a997657a8e835
Vitor Hugo <vitor.hugo@wildlifestudios.com>
2022-01-13 22:51:27 -0300

    Added pydantic model for Item. This is the only model we'll need.

--- al_phonebook/lib.py
+++ al_phonebook/lib.py
@@ -1,7 +1,14 @@
 from abc import ABC, abstractclassmethod, abstractmethod
 from typing import Any, Optional, Sequence, Type
+from pydantic import BaseModel, EmailStr, constr
 DictItem = dict[str, Any]
 OptionalDictItem = DictItem | None
+class Item(BaseModel):
+    name: constr(max_length=100, strip_whitespace=True)  # type: ignore
+    email: Optional[EmailStr]
+    phone_number: Optional[constr(max_length=15, strip_whitespace=True, min_length=8)]  # type: ignore
+
+
 class AbcDatabase(ABC):
     @abstractmethod
     def all(self) -> list[DictItem]:
