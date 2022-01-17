a5d30a2f292be6479c6e1d81bd741fc2ecb2ca1c
Vitor Hugo <vitor.hugo@wildlifestudios.com>
2022-01-16 22:26:53 -0300

    Updated TinyDBDatabase.
    Normalize all inputs parameter to receive and output
    a `DictItem` (a normal dictionary with strings for keys).
    This is important  because the conversion between simple dict and Item models will
    be exclusive done in the `Model` class. All `Database`s should work plain dictionaries.
    
    Moved `get_database` inside as a static method. This lets separate the creation of the database
    for testing (in memory, for example) to the production database (which must be in disk).

--- al_phonebook/lib.py
+++ al_phonebook/lib.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import os
 from abc import ABC, abstractmethod, abstractproperty
 from functools import reduce
@@ -8,6 +9,7 @@ from typing import Any, Optional, Sequence, Type
 from tinydb import TinyDB, where
 from tinydb.queries import QueryInstance
 from tinydb.storages import MemoryStorage
+from .types import DictItem, PathLike, OptionalDictItem
 
 DictItem = dict[str, Any]
 OptionalDictItem = DictItem | None
@@ -73,40 +75,111 @@ class AbcDatabase(ABC):
         raise NotImplementedError()
 
 
+def poorman_fulltext_filter(key: str, value: Any) -> QueryInstance:
+    return where(key).test(
+        lambda entry: str(value).lower().strip() in str(entry).lower()
+        if entry
+        else False
+    )
+
+
+def case_insensitive_filter(key: str, value: Any) -> QueryInstance:
+    return where(key).test(
+        lambda entry: str(value).lower().strip() == str(entry).lower()
+        if entry
+        else False
+    )
+
+
 class TinyDBDatabase(AbcDatabase):
-    def __init__(self, in_memory: bool = False) -> None:
-        self.db = get_database(in_memory)
+    def __init__(self, path: PathLike) -> None:
+        self.db = TinyDBDatabase.get_database(path, False)
+        self.path = Path(path)
         super().__init__()
 
-    def all(self) -> list[DictItem]:
-        r: list[DictItem] = self.db.all()
+    @property
+    def id_field_name(self) -> str:
+        return "doc_id"
+
+    def all(self) -> Sequence[DictItem]:
+        r: Sequence[DictItem] = self.db.all()
+        r: dict[str, Any] = defaultdict(list)
+        for table_name in self.db.tables():
+            for entry in self.db.table(table_name):
+                r[table_name].append(entry)
         return r
 
-    def get(self, id: int) -> OptionalDictItem: 
+    def get(self, id: int, workspace: Optional[str] = None) -> OptionalDictItem:
+        if workspace:
+            r: OptionalDictItem = self.db.table(workspace).get(doc_id=id)
+        else:
         r: OptionalDictItem = self.db.get(doc_id=id)
         return r
 
-    def add_item(self, item: Item) -> int:
+    def add_item(self, item: DictItem, workspace: Optional[str] = None) -> int:
+        if workspace:
+            result: int = self.db.table(workspace).insert(item.dict())
+        else:
         result: int = self.db.insert(item.dict())
         return result
 
-    def add_items(self, items: Sequence[Item]) -> list[int]:
-        ids: list[int] = self.db.insert_multiple((i.dict() for i in items))
+    def add_items(
+        self, items: Sequence[DictItem], workspace: Optional[str] = None
+    ) -> Sequence[int]:
+        ids = []
+        for item in items:
+            ids.append(self.add_item(item, workspace=workspace))
         return ids
 
-    def filter(self, filters: DictItem) -> list[DictItem]:
-        def case_insensitive_query(key: str, value: Any) -> QueryInstance:
-            return where(key).test(lambda entry: value.lower())
+    def filter(
+        self, filters: DictItem, exact: bool = False, workspace: Optional[str] = None
+    ) -> Sequence[DictItem]:
+        """Returns a subset of the items in the phonebook. If exact is True
+        only returns exact matches. By default checks if the values of `filters` are in the
+        entries."""
+        # TODO: Add better search support for various types
 
+        items: Sequence[DictItem] = filters.items()
+
+        if exact:
         query = [
-            case_insensitive_query(field_name, field_value)
-            for field_name, field_value in filters.items()
+                where(field_name) == field_value for field_name, field_value in items
         ]
+        else:
+            query = [
+                poorman_fulltext_filter(field_name, field_value)
+                for field_name, field_value in items
+            ]
+        if workspace:
+            table = self.db.table(workspace)
+        else:
+            table = self.db
 
         # TinyDB accepts multiple queries separated by the boolean operator (__and__)
         # we use reduce to combine multiple queries into one
-        r: list[dict] = self.db.search(reduce(lambda a, b: a & b, query))
+        r: Sequence[dict] = table.search(reduce(lambda a, b: a & b, query))
         return r
+
+    def update(
+        self, id: int, update: DictItem, workspace: Optional[str] = None
+    ) -> Optional[int]:
+        if workspace:
+            table = self.db.table(workspace)
+        else:
+            table = self.db
+        result = table.update(update, doc_ids=[id])
+        return result[0]
+
+    @staticmethod
+    def get_database(path: Optional[PathLike], in_memory: bool = False) -> TinyDB:
+        TinyDB.default_table_name = "personal"
+        if in_memory:
+            return TinyDB(storage=MemoryStorage)
+        try:
+            db = TinyDB(path)
+        except OSError as e:
+            raise DatabasePathError(path)
+        return db
 
 
 class Model:
